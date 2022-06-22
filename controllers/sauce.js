9e28524657efbc2b1ea9cfe0c24ff5fa1462dba1
Hoan Bui-Xuan <hoanbx75@gmail.com>
2022-06-22 22:23:48 +0200

    commit  : sauce 1

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,28 +1,49 @@
 const Sauce = require('../models/sauce');
-
+scriptname = 'controllers/sauce.js : ';
+console.log (scriptname + 'begin '  );
 
 // 1.  CREATE SAUCE 
 
-exports.createSauce = (req, res, next) => {
-    
     exports.createSauce = (req, res, next) => {
-         
+    const funcName =  scriptname + ' - createSauce : ';
+        console.log (funcName + "debut");
+        console.log (funcName  + " req.body = ", req.body);
+
+        const sauceObjet = JSON.parse(req.body.sauce);
+        console.log (funcName  + " req.sauceObjet = ", sauceObjet);
+     
+      //   res.status(201).json({message: "Sauce ajouté !"});
+    
             delete req.body._id
-            console.log(sauceObjet);
-        
+            console.log (funcName  + " req.sauceObjet (after delete _id) = ", sauceObjet);
+      
+      //   let url = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+
+        let url = "http://www.hoanbx.fr/Projects/P3/images/restaurants/jay-wennington-N_Y88TWmGwA-unsplash.jpg";
+        console.log (funcName  + " url  ", url);
+
             const sauce = new Sauce ({
-                ...req.body
+                ...sauceObjet,
+                likes: 0,
+                dislikes:0,
+                imageUrl: url ,
+                usersLiked: [],
+                usersDisliked: [],
             })
-            console.log(sauce);
+            console.log (funcName  + " model Sauce  ", sauce);
+      
+           
             sauce.save()
             .then(() => res.status(201).json({message: "Sauce ajouté !"}))
             .catch((error) => res.status(400).json({error}))
         }
-    }   
+        
+    
+     
 
 // 2. GET ALL SAUCE
 exports.getAllSauce = (req, res, next) => {
- 
+    console.log (scriptname + 'getAllSauce '  );
         Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({error}));
@@ -31,6 +52,7 @@ exports.getAllSauce = (req, res, next) => {
 
 // 3. GET ONE SAUCE 
 exports.getOneSauce = (req, res, next) => {
+    console.log (scriptname + 'getOneSauce '  );
     
         Sauce.findOne({_id: req.params.id})
         .then(sauce => res.status(200).json(sauce))
@@ -58,4 +80,4 @@ exports.upDateSauce = (req, res, next) => {
 
 }
 
-
+console.log (scriptname + 'end '  );
